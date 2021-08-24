40123842142e975c01118384e26a96845af13e84
rajshetye11 <rajshetye11@gmail.com>
2021-08-24 23:20:00 +0530

    [raj] : added 56 30 and 70 in linkedlist

--- src/com/bridgelabz/linkedlist/LinkedListRunner.java
+++ src/com/bridgelabz/linkedlist/LinkedListRunner.java
@@ -2,9 +2,9 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedListRunner {
 	public static void main(String[] args) {
-		MyNode <Integer> firstNode = new MyNode<Integer>(70);
+		MyNode <Integer> firstNode = new MyNode<Integer>(56);
 		MyNode <Integer> secondNode = new MyNode<Integer>(30);
-		MyNode <Integer> thirdNode = new MyNode<Integer>(56);
+		MyNode <Integer> thirdNode = new MyNode<Integer>(70);
 		
 		MyLinkedList linkedlist = new MyLinkedList();
 		linkedlist.add(firstNode);
